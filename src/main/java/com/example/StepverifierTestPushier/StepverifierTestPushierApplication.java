480073eb4d80e291047a1736093d5f64337098c1
arimohy <arimohy@gmail.com>
2021-10-07 20:56:35 -0500

    primer commit ,creacion de servicio y su test

---
+++ src/main/java/com/example/StepverifierTestPushier/StepverifierTestPushierApplication.java
@@ -0,0 +1,13 @@
+package com.example.StepverifierTestPushier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepverifierTestPushierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepverifierTestPushierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StepverifierTestPushier/StepverifierTestPushierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StepverifierTestPushier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepverifierTestPushierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
